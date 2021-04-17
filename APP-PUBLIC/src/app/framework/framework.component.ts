c18725126d1e437e57a72f4dcb61956929aced64
Rajith <rajith.venu-gopal@mocaclinic.ca>
2021-04-16 21:57:57 -0400

    search implementation

--- APP-PUBLIC/src/app/framework/framework.component.ts
+++ APP-PUBLIC/src/app/framework/framework.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { PrimeNGConfig } from 'primeng/api';
+import { Movie } from '../movie';
+import { MovieServiceService } from "../movie-service.service";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-framework',
   templateUrl: './framework.component.html',
   styleUrls: ['./framework.component.css'],
-  providers: [MessageService]
+  providers: [MessageService, MovieServiceService]
 
 })
 export class FrameworkComponent implements OnInit {
@@ -13,43 +16,54 @@ export class FrameworkComponent implements OnInit {
 
   results: string[];
 
-  eventsQuery : string;
+  eventsQuery: string;
 
-  data: any[] = [
-    { label: 'Berlin', value: 'Berlin' },
-    { label: 'Frankfurt', value: 'Frankfurt' },
-    { label: 'Hamburg', value: 'Hamburg' },
-    { label: 'Munich', value: 'Munich' }
-  ]
-  constructor(private messageService: MessageService, private primengConfig: PrimeNGConfig) { }
+  data: any[]
+  // = [
+  //   { label: 'Berlin', value: 'Berlin' },
+  //   { label: 'Frankfurt', value: 'Frankfurt' },
+  //   { label: 'Hamburg', value: 'Hamburg' },
+  //   { label: 'Munich', value: 'Munich' }
+  // ]
+  constructor(private messageService: MessageService, private primengConfig: PrimeNGConfig, private movieService: MovieServiceService, private router : Router) { }
 
-  public pet : string = "";
+  petItemId: any[];
+  public pet: string = "";
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
-
+    this.movieService.getPetItems()
+      .then((movies: Movie[]) => {
+        console.log("movies", movies)
+        this.data = movies.map(movie => {
+          return movie;
+        });
+      });
   }
 
   search(event) {
     console.log(event.query);
     this.eventsQuery = event.query
     this.results = this.data.map(e => {
-      return e.label
+      return e.name
     });
-    console.log(this.results);
+    
+
   }
 
 
   showInfo() {
     this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Under Maintenance' });
   }
-  clear() {
-    this.messageService.clear();
-  }
+
 
   public searchPet(pet): void {
-    console.log("pet",pet);
-   
+
+    this.petItemId = this.data.filter(e =>e.name === pet);
+    console.log("this.petItemId", this.petItemId[0]._id);
+    console.log("pet", pet);
+    this.router.navigateByUrl('/list/'+this.petItemId[0]._id);
+    this.pet = "";
   }
 
 
